06c23e5d3018ea40c85913e0808f4268c0c7622c
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-04-16 18:02:53 -0400

    Add files via upload

---
+++ hash()method.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 16 17:57:31 2021
+
+@author: User
+"""
+
+string = "University of Delaware"
+int = 22.4
+tuple = ('a' , 'b' , 'c')
+
+print('hash value for a string' , hash(string))
+print('hash value for int' , hash(int))
+print('hash value for tuple' , hash(tuple))
