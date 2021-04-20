0bc9d3834cd5471130db99c46c887301a68529e4
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-04-19 22:59:54 -0400

    Add files via upload

---
+++ split()function.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 22:56:33 2021
+
+@author: User
+"""
+
+#Split () method returns a list of the string after splitting the given string by 
+#the specified separator. 
+
+txt1 = "apple#banana#cherry#orange"
+txt2 = "Hello how are you"
+
+result1 = txt1.split("#")
+result2 = txt2.split(" ")
+
+print(result1)
+print(result2)
