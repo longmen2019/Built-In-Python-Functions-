d23f2c0d9a7f27efcc1cfd6cb5d7619f3ec933c1
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-04-19 22:37:36 -0400

    Add files via upload

---
+++ eval()function.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 22:35:17 2021
+
+@author: User
+"""
+#The eval() function lets you evaluate Python expressions from a string-based or compiled-code-based input.
+
+result1 = eval('10+15')
+result2 = eval('3*8')
+
+print(result1)
+print(result2)
