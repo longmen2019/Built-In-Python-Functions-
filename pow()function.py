951647103b3bbd79fb2ef62a50e22ad21d1d476c
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-04-19 23:33:14 -0400

    Add files via upload

---
+++ pow()function.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 23:32:00 2021
+
+@author: User
+"""
+
+#The pow() functions returns the value of x to the power of y.
+
+x = pow (2,3)
+y = pow (3,3)
+
+print(x)
+print(y)
