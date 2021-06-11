79a4777cd19bf4d8a21a927f4d12c315ffabcbda
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-06-11 14:39:07 -0400

    Update hash()method.py

--- hash()method.py
+++ hash()method.py
@@ -9,6 +9,6 @@ string = "University of Delaware"
 int = 22.4
 tuple = ('a' , 'b' , 'c')
 
-print('hash value for a string' , hash(string))
-print('hash value for int' , hash(int))
-print('hash value for tuple' , hash(tuple))
+print('hash value for a string' , hash(string)) #hash value for a string 8922823945743558602
+print('hash value for int' , hash(int)) #hash value for int 922337203685474326
+print('hash value for tuple' , hash(tuple)) #hash value for tuple -1571884274979909749
